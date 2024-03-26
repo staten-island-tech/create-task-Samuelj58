99fd175636e173c08d5fc5e2fe4ec1f9e2bfb013
Samuelj58 <144460795+Samuelj58@users.noreply.github.com>
2024-03-26 09:52:00 -0400

    ww

--- wk.js
+++ wk.js
@@ -1,50 +1,43 @@
-const APIURL= "https://cataas.com/cat"
+const APIURL = "https://cataas.com/cat";
 
 function generateCat() {
-    const catImgContainer = document.getElementById('Catimg');
+  const catImgContainer = document.getElementById("Catimg");
 
-
-    fetch('https://cataas.com/cat')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch cat image');
-            }
-            return response.url;
-        })
-        .then(catImageUrl => {
-
-            catImgContainer.innerHTML = `<img src="${catImageUrl}" alt="Cat Image">`;
-        })
-        .catch(error => {
-            console.error('Error:', error.message);
-        });
+  fetch("https://cataas.com/cat")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch cat image");
+      }
+      return response.url;
+    })
+    .then((catImageUrl) => {
+      catImgContainer.innerHTML = `<img src="${catImageUrl}" alt="Cat Image">`;
+    })
+    .catch((error) => {
+      console.error("Error:", error.message);
+    });
 }
 
+document.getElementById("Rbtn").addEventListener("click", generateCuteat);
 
-document.getElementById('Rbtn').addEventListener('click', generateCuteat);
-
-
-const FatURL= "https://cataas.com/cat/fat"
+const FatURL = "https://cataas.com/cat/fat";
 
 function generateCuteat() {
-    const catImgContainer = document.getElementById('Catimg');
+  const catImgContainer = document.getElementById("Catimg");
 
-
-    fetch('https://cataas.com/cat/cute')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch cat image');
-            }
-            return response.url;
-        })
-        .then(catImageUrl => {
-
-            catImgContainer.innerHTML = `<img src="${catImageUrl}" alt="Cat Image">`;
-        })
-        .catch(error => {
-            console.error('Error:', error.message);
-        });
+  fetch("https://cataas.com/cat/cute")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch cat image");
+      }
+      return response.url;
+    })
+    .then((catImageUrl) => {
+      catImgContainer.innerHTML = `<img src="${catImageUrl}" alt="Cat Image">`;
+    })
+    .catch((error) => {
+      console.error("Error:", error.message);
+    });
 }
 
-
-document.getElementById('Fbtn').addEventListener('click', generateCuteat);
+document.getElementById("Fbtn").addEventListener("click", generateCuteat);
